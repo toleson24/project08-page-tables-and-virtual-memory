c952a16d9d40357b8cb92a031f1084c04068b891
againTraviscodes <t.uni.versity17@gmail.com>
2024-05-15 18:02:34 -0700

    kpages signature corrected

--- xv6-riscv/kernel/defs.h
+++ xv6-riscv/kernel/defs.h
@@ -63,7 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-int             kpages(void);
+uint64          kpages(void);
 
 // log.c
 void            initlog(int, struct superblock*);
