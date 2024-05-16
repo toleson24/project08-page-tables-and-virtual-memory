dde4e65a09c7cdf4d4aee1f38fc21538d5272323
againTraviscodes <t.uni.versity17@gmail.com>
2024-05-15 22:44:19 -0700

    udirs & kdirs syscalls added to syscall table

--- xv6-riscv/kernel/syscall.c
+++ xv6-riscv/kernel/syscall.c
@@ -102,6 +102,8 @@ extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
 extern uint64 sys_kpages(void);
+extern uint64 sys_udirs(void);
+extern uint64 sys_kdirs(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -128,6 +130,8 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_kpages]  sys_kpages,
+[SYS_udirs]   sys_udirs,
+[SYS_kdirs]   sys_kdirs,
 };
 
 void
