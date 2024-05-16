2df973f5e2e537fabdd9caf377a3dee5b2e0f351
againTraviscodes <t.uni.versity17@gmail.com>
2024-05-15 22:39:34 -0700

    udirs partially implemented; TODO test

--- xv6-riscv/kernel/vm.c
+++ xv6-riscv/kernel/vm.c
@@ -437,3 +437,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+int
+kdirs(void)
+{
+  // TODO
+
+}
+
+int
+udirs(pagetable_t pagetable)
+{
+  int c = 0;
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      c += udirs((pagetable_t)child);
+      pagetable[i] = 0;
+    } else if(pte & PTE_V){
+      c++;
+    }
+  }
+  return c;
+}
