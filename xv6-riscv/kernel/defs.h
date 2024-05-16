b48c673b21857d6f1fdfd9422cfa0ae9e9413954
againTraviscodes <t.uni.versity17@gmail.com>
2024-05-15 23:58:31 -0700

    kdirs signature updated

--- xv6-riscv/kernel/defs.h
+++ xv6-riscv/kernel/defs.h
@@ -174,8 +174,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-int             udirs(pagetable_t);
-int             kdirs(pagetable_t);
+uint64          udirs(pagetable_t);
+uint64          kdirs(void);
 
 // plic.c
 void            plicinit(void);
